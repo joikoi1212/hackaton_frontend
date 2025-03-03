bc28f38ef4023d36858e559a629713395bf230f3
joikoi1212 <a.morais0101@gmail.com>
2025-03-03 16:38:02 +0000

    commit

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     turbo: {
-      rules: {
-        "*.mdx": ["mdx-loader"], // Updated from "loaders" to "rules"
-      },
+      loaders: {},
     },
   },
 };
